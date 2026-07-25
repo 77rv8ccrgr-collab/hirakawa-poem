3fc7ab83c0edf33fe87e37cd481c6d871979b7f5
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-07-26 08:52:36 +0900

    60he

--- poemData.js
+++ poemData.js
@@ -124,7 +124,7 @@ const POEM = {
 
     columnGap: 56,
 
-    lineGap: 55,
+    lineGap: 60,
 
     xPositions: [
     980, // 1列目（右端）
@@ -170,7 +170,7 @@ const POEM = {
 
     columnGap: 56,
 
-    lineGap: 55,
+    lineGap: 60,
 
     xPositions: [
     700,
@@ -214,7 +214,7 @@ const POEM = {
 
     columnGap: 56,
 
-    lineGap: 55,
+    lineGap: 60,
 
     xPositions: [
     390,
@@ -261,7 +261,7 @@ const POEM = {
 
     columnGap: 56,
 
-    lineGap: 55,
+    lineGap: 60,
 
     xPositions: [
     -130,
