ab0101be72b2b65e4b86802633a2c23094fce935
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-07-26 22:19:07 +0900

    修正

--- script.js
+++ script.js
@@ -41,10 +41,10 @@ const specialCharacters = {
     "6": { horizontal: true, dx: -14, dy: 2 },
     "7": { horizontal: true, dx: -14, dy: 2 },
     "8": { horizontal: true, dx: -14, dy: 2 },
-    "9": { horizontal: true, dx: -14, dy: 15 },
+    "9": { horizontal: true, dx: -14, dy: 2 },
 
-    "？": { rotate: 90, dx: 19, dy: 8 },
-    "?": { rotate: 90, dx: 19, dy: 8 }
+    "？": { rotate: 90, dx: 19, dy: 12 },
+    "?": { rotate: 90, dx: 19, dy: 12 }
 
 };
 
