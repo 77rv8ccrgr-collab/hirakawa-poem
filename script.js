580a06b331de95bc03b631682d28d0990712f0d2
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-07-28 07:42:03 +0900

    tyousei

--- script.js
+++ script.js
@@ -277,8 +277,8 @@ if ( blockIndex === 3 && columnIndex === 6 ) {
 
 if (blockIndex === 0 && columnIndex === 4) {
 
-    if (index === 8) advance += 28;
-    if (index === 15) advance += 34;
+    if (index === 8) advance += 30;
+    if (index === 15) advance += 32;
 
 }
 
