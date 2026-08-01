053dce55fbaaca4d8f06f9100aeeac41fb2fdc03
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-08-01 21:03:31 +0900

    (24 22

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ const specialCharacters = {
     "8": { horizontal: true, dx: -16, dy: 25 },
     "9": { horizontal: true, dx: -16, dy: 30 },
 
-    "（": { rotate: 90, dx: 18, dy: 22 },
-　　"）": { rotate: 90, dx: 18, dy: 22 },
+    "（": { rotate: 90, dx: 24, dy: 22 },
+　　"）": { rotate: 90, dx: 24, dy: 22 },
    
 　　"?": { sideways: true },
 
