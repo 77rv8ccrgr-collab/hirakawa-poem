1e1eff96a761129c4a9eeeee0da4aefd05f4f949
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-08-02 08:29:45 +0900

    上に

--- poemDataMobile.js
+++ poemDataMobile.js
@@ -92,13 +92,13 @@ const specialCharacters = {
    
 　　"?": { sideways: true },
 
-　　"－": { rotate: 90, dx: 20, dy: 10 },
+　　"－": { rotate: 90, dx: 20, dy: 24 },
 
 "、": { rotate: 0, dx: 28, dy: -32  },
 
-"。": { rotate: -90, dx: -24, dy: 0  },
+"。": { rotate: -90, dx: -24, dy: -4  },
 
-"「": { rotate: 90, dx: 26, dy: 26   },
+"「": { rotate: 90, dx: 24, dy: 22   },
 "」": { rotate: 90, dx: 26, dy: 26   },
 
 };
