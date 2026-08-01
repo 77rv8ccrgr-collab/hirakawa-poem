ae973f5f076b8845148e7fcfdfdc7bf59d024fe2
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-08-01 21:27:02 +0900

    -18 16

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ SVG文字生成
 const specialCharacters = {
 
     "#": { horizontal: true, dx: -16, dy: 10 },
-    "👣": { rotate: -90, dx: -22, dy: 6 },
+    "👣": { rotate: -90, dx: -18, dy: 16 },
     "bar": { horizontal: true, dx: 0, dy: 0 },
 
     "0": { horizontal: true, dx: -14, dy: 2 },
