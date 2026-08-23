89b23f1608a9ba50ba744a66861eff651f06b1b8
77rv8ccrgr-collab <77rv8ccrgr@privaterelay.appleid.com>
2026-08-23 13:02:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -142,23 +142,6 @@ function createCharacter(character) {
         text.style.opacity = 0;
 
     }
-
-if (character.text === "👣") {
-
-    const group = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "g"
-    );
-
-    group.setAttribute(
-        "transform",
-        `rotate(-90 ${x} ${y})`
-    );
-
-    group.appendChild(text);
-
-    return group;
-}
     
     return text;
 
